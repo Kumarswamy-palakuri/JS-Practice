d2dbbc4cabccf5363191b2ac02b1c833b98ce5b6
Your Name <76578469+Kumarswamy-palakuri@users.noreply.github.com>
2024-08-19 11:32:21 +0530

    score Additon

--- Rocl-Paper-Siccor/script.js
+++ Rocl-Paper-Siccor/script.js
@@ -2,21 +2,30 @@
 const comp_choice=['ROCK','PAPER','SICSSORS'];
 let u_ch=document.getElementById('u_choice');
 let c_ch=document.getElementById('c_choice');
-let won=document.getElementById('won')
+let won=document.getElementById('won');
+let y_sc=document.getElementById('y_sc');
+let c_sc=document.getElementById('c_sc');
+let y=0;
+let c=0;
 function game(choice){
   let cmp_guess=comp_choice[Math.floor(Math.random()*comp_choice.length)];
-  console.log(u_ch);
+
   if(choice==='reset')
     {
-      u_ch.innerText=`Your Choice: `;
+      u_ch.innerText=``;
       // u_ch.style.color = 'red'
-      c_ch.innerText=`Computer Choice: `;
+      c_ch.innerText=``;
       won.innerText='';
+      y_sc.innerText='00';
+      c_sc.innerText='00';
+      y=0;
+      c=0;
     }
   else{
-  u_ch.innerText=`Your Choice: ${choice}`;
-  // u_ch.style.color = 'red'
-  c_ch.innerText=`Computer Choice: ${cmp_guess}`;
+  u_ch.innerText=`${choice}`;
+  u_ch.style.color = '#FEFAE0';
+  c_ch.innerText=`${cmp_guess}`;
+  c_ch.style.color = '#FEFAE0';
   if(choice==cmp_guess)
   {
     won.innerText='Game Tie';
@@ -25,26 +34,37 @@ function game(choice){
   else if(choice=='ROCK' && cmp_guess=='SICSSORS'){
     won.innerText='You Won';
     won.style.color="#1B1B1E";
+    y+=1
   }
   else if(choice=='SICSSORS' && cmp_guess=='ROCK'){
     won.innerText='computer Won';
     won.style.color="#1B1B1E";
+    c+=1
   }
   else if(choice=='PAPER' && cmp_guess=='SICSSORS'){
     won.innerText='computer Won';
     won.style.color="#1B1B1E";
+    c+=1
   }
   else if(choice=='SICSSORS' && cmp_guess=='PAPER'){
     won.innerText='you Won';
     won.style.color="#1B1B1E";
+    y+=1
   }
   else if(choice=='ROCK' && cmp_guess=='PAPER'){
     won.innerText='computer Won';
     won.style.color="#1B1B1E";
+    c+=1
   }
   else{
     won.innerText='You Won';
     won.style.color="#1B1B1E";
+   y+=1
   }
+  y_sc.innerText='0'+y;
+  y_sc.style.color="white";
+  c_sc.innerText='0'+c;
+  c_sc.style.color="white";
   }
+
 }
